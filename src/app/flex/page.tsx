3831865d91d88f6f1345250e0b07d17402e8d74b
Jheel Shah <jheelshah510@gmail.com>
2025-06-05 00:18:17 +0530

    first commit

---
+++ src/app/flex/page.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import styles from "./page.module.css"
+
+export default function Home() {
+  return (
+    <div className={styles.event}>
+     <div className={styles.card}>
+      <Image src="/party.jpg" 
+      alt= "Pic"
+      width={500}
+      height={500}
+      />
+      <div className={styles.content}>
+        <h3 className={styles.title}>Lorem ipsum dolor sit amet.</h3>
+        <p className={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam minima ducimus rem quo cumque ipsa recusandae, ab labore tempore atque ad perferendis vel excepturi expedita possimus eius itaque iusto quisquam.</p>
+      </div>
+     </div>
+     <div className={styles.card}>
+      <Image src="/party.jpg" 
+      alt= "Pic"
+      width={500}
+      height={500}
+      />
+      <div className={styles.content}>
+        <h3 className={styles.title}>Lorem ipsum dolor sit amet.</h3>
+        <p className={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam minima ducimus rem quo cumque ipsa recusandae, ab labore tempore atque ad perferendis vel excepturi expedita possimus eius itaque iusto quisquam.</p>
+      </div>
+     </div>
+     <div className={styles.card}>
+      <Image src="/party.jpg" 
+      alt= "Pic"
+      width={500}
+      height={500}
+      />
+      <div className={styles.content}>
+        <h3 className={styles.title}>Lorem ipsum dolor sit amet.</h3>
+        <p className={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam minima ducimus rem quo cumque ipsa recusandae, ab labore tempore atque ad perferendis vel excepturi expedita possimus eius itaque iusto quisquam.</p>
+      </div>
+     </div>
+     <div className={styles.card}>
+      <Image src="/party.jpg" 
+      alt= "Pic"
+      width={500}
+      height={500}
+      />
+      <div className={styles.content}>
+        <h3 className={styles.title}>Lorem ipsum dolor sit amet.</h3>
+        <p className={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam minima ducimus rem quo cumque ipsa recusandae, ab labore tempore atque ad perferendis vel excepturi expedita possimus eius itaque iusto quisquam.</p>
+      </div>
+     </div>
+     <div className={styles.card}>
+      <Image src="/party.jpg" 
+      alt= "Pic"
+      width={500}
+      height={500}
+      />
+      <div className={styles.content}>
+        <h3 className={styles.title}>Lorem ipsum dolor sit amet.</h3>
+        <p className={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam minima ducimus rem quo cumque ipsa recusandae, ab labore tempore atque ad perferendis vel excepturi expedita possimus eius itaque iusto quisquam.</p>
+      </div>
+     </div>
+     <div className={styles.card}>
+      <Image src="/party.jpg" 
+      alt= "Pic"
+      width={500}
+      height={500}
+      />
+      <div className={styles.content}>
+        <h3 className={styles.title}>Lorem ipsum dolor sit amet.</h3>
+        <p className={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam minima ducimus rem quo cumque ipsa recusandae, ab labore tempore atque ad perferendis vel excepturi expedita possimus eius itaque iusto quisquam.</p>
+      </div>
+     </div>
+     
+    </div>
+  );
+}
